be103cd5f544ee41049ceeb4d6b428de7a22f73e
Izabela <izabela.tarasin@gmail.com>
2026-01-07 11:33:43 +0100

    initial project

---
+++ src/main/java/com/IzabelaTarasin/spacebooking/SpaceBookingApplication.java
@@ -0,0 +1,14 @@
+package com.IzabelaTarasin.spacebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBookingApplication.class, args);
+		
+	}
+
+}
